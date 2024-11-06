636296c10e72ef2f01483cacb9d1f911b73279d0
jerry.cj.chen <jerry.cj.chen@linecorp.com>
2024-11-06 20:57:20 +0800

    [refactor] change train

--- src/train.sh
+++
@@ -1,3 +0,0 @@
-# python qlora.py --model_name_or_path "zake7749/gemma-2-2b-it-chinese-kyara-dpo" \
-#                 --learning_rate 0.0008  \
-#                 --per_device_eval_batch_size 2\
--- train.sh
+++ train.sh
@@ -7,7 +7,11 @@ python train.py --base_model_path "zake7749/gemma-2-2b-it-chinese-kyara-dpo" \
                 --epoch 7 \
                 --batch_size 512 \
                 --accum_grad_step 8 \
-                --lr <learning rate> \
+                --lr 2e-4 \
                 --lr_scheduler "cosine" \
                 --warm_up_step 300 \
                 --lora_rank 64
+
+# python qlora.py --model_name_or_path "zake7749/gemma-2-2b-it-chinese-kyara-dpo" \
+#                 --learning_rate 0.0008  \
+#                 --per_device_eval_batch_size 2\
