27c9eff016d0884148bcda5d91f862c29c3d302b
jerry.cj.chen <jerry.cj.chen@linecorp.com>
2024-11-06 20:56:15 +0800

    [refactor] change train

--- train.sh
+++ train.sh
@@ -1,9 +1,5 @@
 # #!/bin/bash
 
-# python qlora.py --model_name_or_path "zake7749/gemma-2-2b-it-chinese-kyara-dpo" \
-#                 --learning_rate 0.0008  \
-#                 --per_device_eval_batch_size 2\
-
 python train.py --base_model_path "zake7749/gemma-2-2b-it-chinese-kyara-dpo" \
                 --train_data_path data/train.json \
                 --valid_data_path data/public_test.json \
