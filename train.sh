b730a439a1b48b938169756c5a6938a8cf6d0007
Kogby <kogby0507@gmail.com>
2024-11-08 01:20:07 +0800

    refactor

--- train.sh
+++ train.sh
@@ -3,7 +3,7 @@
 python qlora.py --dataset data/train.json \
                 --model_name_or_path zake7749/gemma-2-2b-it-chinese-kyara-dpo \
                 --learning_rate 0.0008  \
-                --per_device_eval_batch_size 128\
-                --per_device_train_batch_size 256\
+                --per_device_eval_batch_size 32\
+                --per_device_train_batch_size 64\
 
 # Option: "yentinglin/Llama-3-Taiwan-8B-Instruct"
