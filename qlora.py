3d2d1cd960a269255f6c3edcbae207d13b1ab054
jerry.cj.chen <jerry.cj.chen@linecorp.com>
2024-11-07 16:41:24 +0800

    req for qlora

--- qlora.py
+++ qlora.py
@@ -346,15 +346,16 @@ def get_accelerate_model(args, checkpoint_dir):
     model.config.torch_dtype=(torch.float32 if args.fp16 else (torch.bfloat16 if args.bf16 else torch.float32))
 
     # # Tokenizer
-    tokenizer = AutoTokenizer.from_pretrained(
-        args.model_name_or_path,
-        cache_dir=args.cache_dir,
-        padding_side="right",
-        use_fast=False, # Fast tokenizer giving issues.
-        tokenizer_type='llama' if 'llama' in args.model_name_or_path else 'gemma', # Needed for HF name change
-        trust_remote_code=args.trust_remote_code,
-        use_auth_token=args.use_auth_token,
-    )
+    # tokenizer = AutoTokenizer.from_pretrained(
+    #     args.model_name_or_path,
+    #     cache_dir=args.cache_dir,
+    #     padding_side="right",
+    #     use_fast=False, # Fast tokenizer giving issues.
+    #     tokenizer_type='llama' if 'llama' in args.model_name_or_path else 'gemma', # Needed for HF name change
+    #     trust_remote_code=args.trust_remote_code,
+    #     use_auth_token=args.use_auth_token,
+    # )
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path)
 
     if tokenizer._pad_token is None:
         smart_tokenizer_and_embedding_resize(
